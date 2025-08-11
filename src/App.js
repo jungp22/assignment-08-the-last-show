6ea3cb36f861bd93dcaa2f99acf594f5d0362761
Jung-Hwan Park <jungpark.2002@gmail.com>
2025-08-11 16:30:52 -0600

    styling changes, removed placeholders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Header from './Components/Header';
-import EmptyObituary from './Components/EmptyObituary';
-import Main from './Components/Main';
+import "./App.css";
+import Header from "./Components/Header";
+import EmptyObituary from "./Components/EmptyObituary";
+import Main from "./Components/Main";
+import AddObituary from "./Components/AddObituary";
 function App() {
   return (
     <>
-      <Header/>
-      <Main/>
+      <Header />
+      <Main />
+      <AddObituary />
     </>
   );
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,7 +6,6 @@ import EmptyObituary from "./EmptyObituary";
 const Main = () => {
   return (
     <div className="Main">
-        <p>test</p>
       <EmptyObituary />
     </div>
   );
